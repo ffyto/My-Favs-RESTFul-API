cbd688379c99a42a435cd1e0abf6c85e0678d074
Mauro Arango <cmauricio.arango@gmail.com>
2022-09-04 23:01:36 -0500

    initial cofiguration

---
+++ routes.js
@@ -0,0 +1,11 @@
+const user = require("./api/users/users.routes");
+
+function routes(app) {
+	app.use("/api/healthcheck", healthcheck);
+	app.use("/api/users", user);
+
+	// auth routes
+	// app.use('/auth/local', authLocal);
+}
+
+module.exports = routes;
