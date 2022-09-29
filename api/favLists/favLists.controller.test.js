0e3255bf71e8f54980c2455aa2374ac0d24933e5
Mauro Arango <cmauricio.arango@gmail.com>
2022-09-29 11:28:01 -0500

    includes the suggestions made by the tutors

--- api/favLists/favLists.controller.test.js
+++ api/favLists/favLists.controller.test.js
@@ -12,7 +12,7 @@ const login = {
 
 describe('favLists', () => {
   beforeAll(async () => {
-    await mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+    mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
   });
 
   afterAll(async () => {
@@ -21,51 +21,63 @@ describe('favLists', () => {
   });
 
   test('should return status 200 and get all favLists in the DB', async () => {
-    const { statusCode } = await request(app).get('/api/favLists');
+    const jwt = await signToken(login);
+    const response = await request(app)
+      .get('/api/favLists')
+      .set('Authorization', `Bearer ${jwt}`);
 
-    expect(statusCode).toBe(200);
+    expect(response.statusCode).toBe(200);
+    expect(response.body.message).toMatch('Showing all favLists');
   });
 
   test('should return status 200 and get a single favList', async () => {
-    const id = '63214683ca34ef553baf579e';
+    const id = '6335c1f58b4d9bd6accf2585';
+    const jwt = await signToken(login);
 
-    const { statusCode } = await request(app).get(`/api/favLists/${id}`);
+    const response = await request(app)
+      .get(`/api/favLists/${id}`)
+      .set('Authorization', `Bearer ${jwt}`);
 
-    expect(statusCode).toBe(200);
+    expect(response.statusCode).toBe(200);
+    expect(response.body.message).toMatch(`Showing favList id:${id}`);
   });
 
   test('should return status 200 and get all user favLists', async () => {
     const jwt = await signToken(login);
-    console.log(
-      '🚀 ~ file: favLists.controller.test.js ~ line 39 ~ test ~ jwt',
-      jwt
-    );
 
-    const { statusCode } = await request(app)
-      .get('/api/favs')
+    const response = await request(app)
+      .get('/api/favLists/user')
       .set('Authorization', `Bearer ${jwt}`);
-    expect(statusCode).toBe(200);
+    expect(response.statusCode).toBe(200);
+    expect(response.body.message).toMatch('Showing all user favLists');
   });
 
   test('should return status 201 and create a favList', async () => {
     const jwt = await signToken(login);
     const data = {
-      name: `My favorite songs version ${Math.floor(Math.random() * 1000)}`,
+      name: `My favorite songs version ${Math.floor(Math.random() * 10000)}`,
     };
-    const { statusCode } = await request(app)
+    const response = await request(app)
       .post('/api/favLists/')
       .send(data)
       .set('Authorization', `Bearer ${jwt}`);
-    expect(statusCode).toBe(201);
+
+    expect(response.statusCode).toBe(201);
+    expect(response.body.message).toMatch(`FavList successfuly created`);
   });
 
   test('should return status 200 and delete a favList', async () => {
     const jwt = await signToken(login);
-    const id = '632298b732ee40c77a2c74c0';
+    const id =
+      '633473c639a3a18d7cbeb867' ||
+      '6335c2b625cfbede484d023d' ||
+      '6335c2d8907415b91a1561a8' ||
+      '6335c1f58b4d9bd6accf2585';
 
-    const { statusCode } = await request(app)
+    const response = await request(app)
       .delete(`/api/favLists/${id}`)
       .set('Authorization', `Bearer ${jwt}`);
-    expect(statusCode).toBe(200);
+    expect(response.statusCode).toBe(200);
+    expect(response.body.message).toMatch(`FavList eliminated`);
   });
 });
--- routes.js
+++ routes.js
@@ -3,13 +3,14 @@ const healthcheck = require('./api/healthcheck/healthcheck.routes');
 const authLocal = require('./auth/local/local.routes');
 const favList = require('./api/favLists/favLists.routes');
 const fav = require('./api/favs/favs.routes');
+const { isAuthenticated } = require('./auth/auth.services');
 
 function routes(app) {
   app.use('/api/healthcheck', healthcheck);
   app.use('/api/users', user);
-  app.use('/api/favLists', favList);
+  app.use('/api/favLists', isAuthenticated, favList);
   app.use('/auth/local', authLocal);
-  app.use('/api/favs', fav);
+  app.use('/api/favs', isAuthenticated, fav);
 }
 
 module.exports = routes;
